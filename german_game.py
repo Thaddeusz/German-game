3bff119b43027a19261fe838787e85161f1cbd4f
Thaddeusz <thaddesu@gmail.com>
2020-11-16 20:35:22 +0000

    Adding check for input, adding feature to write how many words you knew out of how many you tried and writing out the incorrect words at the end

--- german_game.py
+++ german_game.py
@@ -10,10 +10,18 @@ import random
 with open('all.txt','r') as inf:
     dictionary = eval(inf.read())
 
-
+correct_words = 0
+number_of_words = 0
 incorrect_words = []
+while True:
+    try:
+        language = input(f"Welcome to the German dictionary game, currently there are {len(list(dictionary.keys()))} words. \nYou can exit by writing exit or quit and also write show or pass to show the current word. \nWould you like German-English(g) or English-German(e)? ")
+        if language[0].lower() == "g" or language[0].lower() == "e":
+            break
+    except:
+        pass
 
-language = input(f"Welcome to the German dictionary game, currently there are {len(list(dictionary.keys()))} words. \nYou can exit by writing exit or quit and also write show or pass to show the current word. \nWould you like German-English(g) or English-German(e)? ")
+
 #if language[0].lower() != "g" or language[0].lower() != "e":
 #    language = input("please write either e or g: ")
 
@@ -29,11 +37,14 @@ while True:
 
     inp = input("\nWhat's the word in the other language? ")
     # compares answer with value
+    number_of_words += 1
     if inp == list(dictionary.values())[item_number]:
         print("\x1b[6;30;42m" + "CORRECT" + "\x1b[0m")
-
+        correct_words += 1
     elif inp == "exit" or inp == "quit":
-            print(incorrect_words)    
+            result = correct_words / number_of_words * 100
+            print(f"You knew {correct_words} correct words out of {number_of_words}! That's {result}%! ")    
+            print(f"You did not know these words\n {incorrect_words}")
             break
     else:
         print(f"You are incorrect, the correct word is: \033[1;32;40m {(list(dictionary.values())[item_number])} \x1b[0m \n")
