82ebc6f3d070106fea984b789b91981bbe27ab98
Thaddeusz <thaddesu@gmail.com>
2020-11-17 13:50:18 +0000

    adding option to select other wordlist

--- german_game.py
+++ german_game.py
@@ -6,21 +6,39 @@
 
 import random
 
-# imports the dictionary into the variable dictionary with eval, that basically just copies shit 1:1
-with open('all.txt','r') as inf:
-    dictionary = eval(inf.read())
+# imports the dictionary into the variable dictionary with eval, that basically just copies it into a variable 1:1
 
 correct_words = 0
 number_of_words = 0
 incorrect_words = []
+
 while True:
     try:
-        language = input(f"Welcome to the German dictionary game, currently there are {len(list(dictionary.keys()))} words. \nYou can exit by writing exit or quit and also write show or pass to show the current word. \nWould you like German-English(g) or English-German(e)? ")
+        language = input(f"Welcome to the German dictionary game, you can exit by writing exit or quit. \nWould you like German-English(g) or English-German(e)? ")
         if language[0].lower() == "g" or language[0].lower() == "e":
             break
     except:
         pass
 
+while True:
+    try:
+        topic = input("Which topic would you like? Food & Animals(f) or everything(e)? ")
+        if topic[0].lower() == "f" or topic[0].lower() == "e":
+            break
+    except:
+        pass
+        
+if topic[0].lower() == "f":
+    topic_file = "food_animals.txt"
+elif topic[0].lower() == "e":
+    topic_file = "all.txt"
+else:
+    pass
+
+with open(topic_file,'r') as inf:
+    dictionary = eval(inf.read())
+
+print(f"currently there are {len(list(dictionary.keys()))} words")
 
 #if language[0].lower() != "g" or language[0].lower() != "e":
 #    language = input("please write either e or g: ")
@@ -29,25 +47,34 @@ while True:
 if language[0].lower() == "g":  dictionary = {v: k for k, v in dictionary.items()}
 
 while True:
-    # generate a random number
-    item_number = random.randrange(0,len(list(dictionary.keys())))
-    # selects the key appropiate to the random number
-    key_word = list(dictionary.keys())[item_number]
-    print(key_word)
+    try:
+        # generate a random number
+        item_number = random.randrange(0,len(list(dictionary.keys())))
+        # selects the key appropiate to the random number
+        key_word = list(dictionary.keys())[item_number]
+        number_of_words += 1
+        print(f"{number_of_words}. {key_word}")
 
-    inp = input("\nWhat's the word in the other language? ")
-    # compares answer with value
-    number_of_words += 1
-    if inp == list(dictionary.values())[item_number]:
-        print("\x1b[6;30;42m" + "CORRECT" + "\x1b[0m")
-        correct_words += 1
-    elif inp == "exit" or inp == "quit":
-            result = correct_words / number_of_words * 100
-            print(f"You knew {correct_words} correct words out of {number_of_words}! That's {result}%! ")    
-            print(f"You did not know these words\n {incorrect_words}")
-            break
-    else:
-        print(f"You are incorrect, the correct word is: \033[1;32;40m {(list(dictionary.values())[item_number])} \x1b[0m \n")
-        incorrect_words.append(list(dictionary.items())[item_number])
-        # collects incorrect words
+        inp = input("\nWhat's the word in the other language? ")
+        # compares answer with value
+        
+        if inp == list(dictionary.values())[item_number]:
+            print("\x1b[6;30;42m" + "CORRECT" + "\x1b[0m")
+            correct_words += 1
+            dictionary.pop(key_word)
+        elif inp == "exit" or inp == "quit":
+                result = correct_words / number_of_words * 100
+                print(f"You knew {correct_words} correct words out of {number_of_words}! That's {result}%! ")    
+                print(f"You did not know these words\n {incorrect_words}")
+                break
+        else:
+            print(f"You are incorrect, the correct word is: \033[1;32;40m {(list(dictionary.values())[item_number])} \x1b[0m \n")
+            incorrect_words.append(list(dictionary.items())[item_number])
+            # collects incorrect words
+    except ValueError:
+        result = correct_words / number_of_words * 100
+        print(f"You knew {correct_words} correct words out of {number_of_words}! That's {result}%! ")
+        break
+
+    
             
